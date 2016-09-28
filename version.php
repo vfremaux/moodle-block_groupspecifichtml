25273e5e17f268ed55054fc3928ac45045725b3f
valery <valery.fremaux@gmail.com>
2016-09-28 11:38:11 +0200

    automated sync from SVN

--- version.php
+++ version.php
@@ -15,18 +15,22 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Version details
+ * Version details.
  *
- * @package    block
- * @subpackage groupspecifichtml
- * @copyright  2012 valery fremaux (valery.fremaux@gmail.com)
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @package     block_groupspecifichtml
+ * @category    blocks
+ * @author      Valery Fremaux <valery.fremaux@gmail.com>
+ * @copyright   2012 Valery Fremaux (valery.fremaux@gmail.com)
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2014082600;        // The current plugin version (Date: YYYYMMDDXX)
-$plugin->requires  = 2014042900;        // Requires this Moodle version
+$plugin->requires  = 2014110400;        // Requires this Moodle version
 $plugin->component = 'block_groupspecifichtml';  // Full name of the plugin (used for diagnostics)
 $plugin->maturity = MATURITY_RC;
-$plugin->release = '2.7.0 (build 2014082600)';
+$plugin->release = '2.8.0 (build 2014082600)';
+
+// Non moodle attributes.
+$plugin->codeincrement = '2.8.0000';
