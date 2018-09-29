01a673e3d2897ecfd2280fce9a213b54ced5f6c9
valery <valery.fremaux@gmail.com>
2018-09-29 12:27:17 +0200

    Fix GRDP

--- lang/fr/block_groupspecifichtml.php
+++ lang/fr/block_groupspecifichtml.php
@@ -1,6 +1,29 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-$string['groupspecifichtml:addinstance'] = 'Peut ajouter une instance';
+/**
+ * @package   block_groupspecifichtml
+ * @copyright 2015 Valery Fremaux
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['groupspecifichtml:addinstance'] = 'Ajouter une instance au cours';
+
+// Privacy.
+$string['privacy:metadata'] = 'Le bloc HTML (par groupe) ne détient directement aucune donnée relative aux utilisateurs.';
 
 $string['blockname'] = 'HTML (par groupe)';
 $string['pluginname'] = 'HTML (par groupe)';
